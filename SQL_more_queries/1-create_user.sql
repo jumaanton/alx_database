6c07276cfbff46ec0b8c021f7cd0aa6b9e42aad8
Juma Antony <jumaantony840@gmail.com>
2023-08-17 10:28:50 +0300

    tweaked task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '${new_user_password}';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY '${new_user_password}';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
