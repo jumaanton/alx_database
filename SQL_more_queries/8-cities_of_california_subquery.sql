f2afea438a226f7897c0dcce6fc393efae1631fd
Juma Antony <jumaantony840@gmail.com>
2023-08-17 11:37:58 +0300

    cities_of_california_subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- list all cities contained in the database hbtn_0d_usa
 -- The database hbtn_0d_usa will contain:
-USE hbtn_0d_usa;
-SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = 'California';
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
